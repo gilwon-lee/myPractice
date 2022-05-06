85ec9e69cb13d2ab3a4a92d5cdc03ac4fc192139
lee <bluesky930401@gmail.com>
2022-05-06 13:14:47 +0900

    220506 commit

---
+++ JS/app.js
@@ -0,0 +1,47 @@
+// const a = 5 ;
+// const b = 2 ;
+// let name = "lee";
+
+// console.log(name);
+// console.log(a+b);
+// console.log(a*b);
+// console.log(a/b);
+
+// name = "gilwon";
+// console.log(name);
+
+// const dayOfWeek = ["mon","tue","wed","thu","fri","sat"];
+
+// console.log(dayOfWeek[5]);
+
+// // array 값 추가
+// dayOfWeek.push("sun");
+
+
+// console.log(dayOfWeek)
+
+// const apple ="apple";
+// const potato = "potato";
+
+// const toBuy = ["apple","potato","pizze"];
+// toBuy.push("kimbab");
+// console.log(toBuy);
+
+
+const player = {
+    name : "lee",
+    points : 10,
+    fat:true,
+};
+
+console.log(player);
+console.log(player.name);
+console.log(player["name"]);
+
+player.fat =false;
+player.lastName = "potato";
+console.log(player);
+
+
+
+
