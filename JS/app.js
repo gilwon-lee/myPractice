a9636d6ad0068992a525b2ba5949af25b060277d
lee <bluesky930401@gmail.com>
2022-05-06 15:53:27 +0900

    220506-2 commit

--- JS/app.js
+++ JS/app.js
@@ -1,3 +1,5 @@
+// JS 복습하기 
+
 // const a = 5 ;
 // const b = 2 ;
 // let name = "lee";
@@ -28,20 +30,46 @@
 // console.log(toBuy);
 
 
+// const player = {
+//     name : "lee",
+//     points : 10,
+//     fat:true,
+// };
+
+// console.log(player);
+// console.log(player.name);
+// console.log(player["name"]);
+
+// player.fat =false;
+// player.lastName = "potato";
+// console.log(player);
+
+function sayHello(name,age){
+    console.log("hello my name is "+name+" i'm"+age+" years old");
+}
+sayHello("a",10);
+sayHello("b",15);
+sayHello("c",20);
+
 const player = {
     name : "lee",
-    points : 10,
-    fat:true,
-};
+    sayHello:function(otherPerson){
+        console.log("hello!"+otherPerson);
+    }
+}
 
-console.log(player);
 console.log(player.name);
-console.log(player["name"]);
+player.sayHello("lynn");
 
-player.fat =false;
-player.lastName = "potato";
-console.log(player);
+function plus(a,b){
+    return console.log(a+b);
+}
 
+plus(10,20);
+function divide(a,b){
+    return console.log(a/b);
+}
+divide(10,3);
 
 
 
