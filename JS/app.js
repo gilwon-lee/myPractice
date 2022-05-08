a947296f4edc92471eaa8fd7abfd1e800ba83db2
lee <bluesky930401@gmail.com>
2022-05-08 20:48:41 +0900

    02020508 commit

--- JS/app.js
+++ JS/app.js
@@ -44,32 +44,87 @@
 // player.lastName = "potato";
 // console.log(player);
 
-function sayHello(name,age){
-    console.log("hello my name is "+name+" i'm"+age+" years old");
-}
-sayHello("a",10);
-sayHello("b",15);
-sayHello("c",20);
+// function sayHello(name,age){
+//     console.log("hello my name is "+name+" i'm"+age+" years old");
+// }
+// sayHello("a",10);
+// sayHello("b",15);
+// sayHello("c",20);
 
-const player = {
-    name : "lee",
-    sayHello:function(otherPerson){
-        console.log("hello!"+otherPerson);
-    }
+// const player = {
+//     name : "lee",
+//     sayHello:function(otherPerson){
+//         console.log("hello!"+otherPerson);
+//     }
+// }
+
+// console.log(player.name);
+// player.sayHello("lynn");
+
+// function plus(a,b){
+//     return console.log(a+b);
+// }
+
+// plus(10,20);
+// function divide(a,b){
+//     return console.log(a/b);
+// }
+// divide(10,3);
+
+//1+1 1-1 a+b a-b  a/b a**b
+
+// const calculator = {
+//     add : function(a,b){
+//         console.log(a+b);
+//     },
+//     minus : function(a,b){
+//         console.log(a-b);
+//     },
+//     divide : function(a,b){
+//         console.log(a/b);
+//     },
+//     powerof : function(a,b){
+//         console.log(a**b);
+//     }
+// }
+// calculator.add(1,2);
+// calculator.minus(1,2);
+// calculator.powerof(1,2);
+// calculator.divide(1,2);
+// const age = 96;
+// function calculatorKrAge(ageOfForeigner){
+//     return ageOfForeigner +2;
+// }
+// const krAge = calculatorKrAge(age);
+// console.log(krAge);
+
+//parseInt : type 변경함수
+const age = parseInt(prompt("how old are you"));
+// console.log(typeof age);
+// console.log(age);
+//isNaN() = 
+// console.log(isNaN(age));
+
+// 자바 스크립트 조건문 : 자바랑 비슷함
+// if(condition){
+//     code
+// }else{
+//}
+if(isNaN(age)){
+    console.log("Please write a number");
+}else if (0< age < 18){
+    console.log("you are too young");
+}else{
+    console.log("you can drink");
 }
 
-console.log(player.name);
-player.sayHello("lynn");
+ 
 
-function plus(a,b){
-    return console.log(a+b);
-}
 
-plus(10,20);
-function divide(a,b){
-    return console.log(a/b);
-}
-divide(10,3);
+
+
+
+
 
 
 
