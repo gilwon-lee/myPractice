cfbd62f383f3dad137ac8f23a467ab18f9ac7737
lee <bluesky930401@gmail.com>
2022-05-20 23:06:41 +0900

    220520-2 commit

--- webpro/src/main/java/com/newLecture/web/controller/IndexController.java
+++ webpro/src/main/java/com/newLecture/web/controller/IndexController.java
@@ -11,7 +11,7 @@ public class IndexController implements Controller{
 	@Override
 	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		// TODO Auto-generated method stub
-		ModelAndView mv = new ModelAndView("index");
+		ModelAndView mv = new ModelAndView("root.index");
 		mv.addObject("data","Spring MVC");
 //		mv.setViewName("/WEB-INF/view/index.jsp");
 		return mv;
--- webpro/src/main/java/com/newLecture/web/controller/notice/DetailController.java
+++ webpro/src/main/java/com/newLecture/web/controller/notice/DetailController.java
@@ -11,7 +11,7 @@ public class DetailController implements Controller{
 	@Override
 	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		// TODO Auto-generated method stub
-		ModelAndView mv = new ModelAndView("notice/detail");
+		ModelAndView mv = new ModelAndView("notice.detail");
 //				mv.setViewName("/WEB-INF/notice/index.jsp");
 		return mv;
 				
