a2342a3e299566d138bd678d9e6071f709550968
lee <bluesky930401@gmail.com>
2022-05-21 16:34:02 +0900

    220521 commit

---
+++ webpro/src/main/java/com/newLecture/web/controller/HomeController.java
@@ -0,0 +1,24 @@
+package com.newLecture.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping("/")
+public class HomeController{
+
+	@RequestMapping("index")
+	public String index(){
+		return "root.index";
+	}
+	
+//	@Override
+//	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+//		// TODO Auto-generated method stub
+//		ModelAndView mv = new ModelAndView("root.index");
+//		mv.addObject("data","Spring MVC");
+//		mv.setViewName("/WEB-INF/view/index.jsp");
+//		return mv;
+//	}
+}
