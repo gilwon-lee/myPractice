8fbfef452073c76fb3d838f9eddb2117876b1a48
lee <bluesky930401@gmail.com>
2022-05-27 21:26:44 +0900

    220527 commit

--- webpro/src/main/java/com/newLecture/web/controller/HomeController.java
+++ webpro/src/main/java/com/newLecture/web/controller/HomeController.java
@@ -1,16 +1,31 @@
 package com.newLecture.web.controller;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 
 @Controller
 @RequestMapping("/")
 public class HomeController{
 
+	//@RequestMapping("index")
+	/* @ResponseBody public void index(HttpServletResponse response) throws
+	 * IOException{ PrintWriter out = response.getWriter();
+	 * out.println("hello index"); // return "root.index"; }
+	 */
+	
 	@RequestMapping("index")
-	public String index(){
-		return "root.index";
+	//@ResponseBody -> tiles,Annotation에서 패턴을 찾는 것이 아닌 아닌 
+	//출력 자체를  할때 사용 (hello index라는 text 출력)
+	@ResponseBody
+	public String index() {
+	return "hello index";
 	}
 	
 //	@Override
