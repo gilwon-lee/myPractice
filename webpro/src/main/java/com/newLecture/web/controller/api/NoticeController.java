67393f793c749ed6b143a9ac3a3668e5f9504d6d
lee <bluesky930401@gmail.com>
2022-05-31 22:23:17 +0900

    220531 commit

--- webpro/src/main/java/com/newLecture/web/controller/api/NoticeController.java
+++ webpro/src/main/java/com/newLecture/web/controller/api/NoticeController.java
@@ -1,7 +1,14 @@
 package com.newLecture.web.controller.api;
 
+import java.sql.SQLException;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.newLecture.web.entitiy.Notice;
+import com.newLecture.web.service.NoticeService;
 
 
 // @RestController -> RequestBody와 같은 
@@ -10,9 +17,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/notice/")
 public class NoticeController {
 	
+	@Autowired
+	private NoticeService service;
+	
+	//객체를 던지면 알아서 JSON 형식으로 바뀐다.
 	@RequestMapping("list")
-	public String list() {
+	public Notice list() throws ClassNotFoundException, SQLException {
+		List<Notice> list = service.getList(1,"title", "");
 		
-		return "공지사항 list";
+		return list.get(0);
 	}
 }
