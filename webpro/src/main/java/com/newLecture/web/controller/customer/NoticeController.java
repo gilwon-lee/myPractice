07c10a8dabc5d29c31006c91b69db3a109ff7a84
lee <bluesky930401@gmail.com>
2022-06-02 21:05:18 +0900

    220602 commit

--- webpro/src/main/java/com/newLecture/web/controller/customer/NoticeController.java
+++ webpro/src/main/java/com/newLecture/web/controller/customer/NoticeController.java
@@ -3,9 +3,12 @@ package com.newLecture.web.controller.customer;
 import java.sql.SQLException;
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.newLecture.web.entitiy.Notice;
 import com.newLecture.web.service.NoticeService;
@@ -18,8 +21,11 @@ public class NoticeController {
 	private NoticeService noticeService;
 	
 	@RequestMapping("list")
-	public String list() throws ClassNotFoundException, SQLException {
-		List<Notice> list = noticeService.getList(1, "TITLE","");
+	//p -> page 로 변환
+	public String list(@RequestParam(defaultValue = "1",name ="p") int page) throws ClassNotFoundException, SQLException {
+		//String p = request.getParameter("p");
+		System.out.println(page);
+		//List<Notice> list = noticeService.getList(1, "TITLE","");
 		return "notice.list";
 	}
 	
